da3a05ede950c18ee34067a03b140b0d6223135b
monirul-fahad <immonirulfahad@gmail.com>
2021-12-18 21:48:32 +0600

    [ADD] product

---
+++ src/Pages/Home/Products/Products/Products.js
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from "react";
+import Product from "../Product/Product";
+import "./Products.css";
+
+const Products = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/products")
+      .then((res) => res.json())
+      .then((data) => setProducts(data));
+  }, []);
+  return (
+    <div className="py-5 glass-container">
+      <div className="py-5 container ">
+        <div className="mb-5">
+          <h1>Featured Glasses</h1>
+        </div>
+        <div className="row row-cols-1 row-cols-md-3">
+          {products.map((product) => (
+            <Product key={product._id} product={product}></Product>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Products;
